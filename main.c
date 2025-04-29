17b3034ee868aa12b0615dffa7f53c4e85d6104b
Sinyster <martasko6@protonmail.com>
2025-04-29 21:42:30 +0200

    New Layout Concept

--- main.c
+++ main.c
@@ -1,4 +1,13 @@
 #include "raylib.h"
+#include <stdio.h>
+
+typedef struct {
+  double money;
+} GameData;
+typedef struct {
+} Battery;
+typedef struct {
+} Generator;
 
 int main(void) {
   // App Variables
@@ -18,17 +27,73 @@ int main(void) {
 
   // Color Pallete
   Color bgColor = WHITE;
-  Color headerColor = {};
-  Color textColor = {};
-  Color linesColor = {};
+  Color bgColor2 = LIGHTGRAY;
+  Color hoverBg = GRAY;
+  Color headerColor = DARKGRAY;
+  Color textColor = GRAY;
+  Color linesColor = GRAY;
+
+  // DEFAULT VARIABLES
+  GameData data = {0};
+  Battery bat = {};
+  Generator gen = {};
+
+  // Panel Variables
+  const int PanelHeight = 35;
+  const int headerFontSize = 20;
+
+  // Game Variables;
+  bool isHovering = false;
+  bool isClicked = false;
 
   while (!WindowShouldClose()) {
     // Base Functions for All Scenes
+    Vector2 mousePoint = GetMousePosition();
     BeginDrawing();
     ClearBackground(bgColor);
 
-    
+#pragma region Upper Panel
+    DrawRectangle(0, 0, screenWidth, PanelHeight, bgColor2);
 
+    // First Part
+    DrawLine(screenWidth / 5, PanelHeight * 0.1, screenWidth / 5,
+             PanelHeight * 0.9, linesColor);
+    Rectangle mainSceneButton = {3, PanelHeight * 0.1, screenWidth / 5 - 6,
+                                 PanelHeight * 0.8};
+    isHovering = CheckCollisionPointRec(mousePoint, mainSceneButton);
+    isClicked = isHovering && IsMouseButtonPressed(MOUSE_LEFT_BUTTON);
+    DrawRectangleRec(mainSceneButton, isHovering ? hoverBg : bgColor2);
+    DrawText("Game",
+             (screenWidth / 5) / 2 - MeasureText("Game", headerFontSize) / 2,
+             PanelHeight / 2 - headerFontSize / 2, headerFontSize, headerColor);
+
+    if (currentScene != SCENE_MAIN && isClicked) {
+      currentScene = SCENE_MAIN;
+    }
+    // Second Part
+    // Third Part
+    // Forth Part
+    // Fifth Part
+
+#pragma endregion
+    if (currentScene == SCENE_MAIN) {
+#pragma region Lower Panel
+      DrawRectangle(0, screenHeight - PanelHeight, screenWidth, PanelHeight,
+                    bgColor2);
+      // Drawing Money
+      char moneyText[30];
+      snprintf(moneyText, sizeof(moneyText), "$ %0.2f", data.money);
+      DrawLine(screenWidth / 5,
+               (screenHeight - PanelHeight) + PanelHeight * 0.1,
+               screenWidth / 5,
+               (screenHeight - PanelHeight) + PanelHeight * 0.9, linesColor);
+      DrawText(moneyText,
+               (screenWidth / 5) / 2 -
+                   MeasureText(moneyText, headerFontSize) / 2,
+               screenHeight - PanelHeight / 2 - headerFontSize / 2,
+               headerFontSize, headerColor);
+#pragma endregion
+    }
 
     EndDrawing();
   }
