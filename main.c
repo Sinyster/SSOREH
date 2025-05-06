2d1e1d743e40e65a2cfeb7d38de0372c6b4a8f88
Sinyster <martasko6@protonmail.com>
2025-05-06 13:20:41 +0200

    Added Upper Panel

--- main.c
+++ main.c
@@ -1,8 +1,11 @@
+#include "functions.h"
 #include "raylib.h"
-#include <stdio.h>
+
+// #include <stdio.h>
 
 typedef struct {
   double money;
+  float sunlight;
 } GameData;
 
 int main(void) {
@@ -16,8 +19,18 @@ int main(void) {
   SetTargetFPS(targetFps);
   // SetExitKey(KEY_NULL);
 
+  // Day/Night Variables
+  const int dayMinutes = 10;
+  const int nightMinutes = 5;
+  float sunlight = 0.0f;
+  bool isDay = true;
+
+  // Setting Screens
   typedef enum { SCREEN_MAIN } GameScreen;
   GameScreen currentScreen = SCREEN_MAIN;
+
+  // Timer Variables
+  float dayTimer = 0.0f;
 
   // Colors
   Color bg1 = WHITE;
@@ -37,9 +50,77 @@ int main(void) {
   const int HeaderFont = 30;
   const int textFont = 22;
 
+  // Functional Variables
+  bool isHovering = false;
+  bool isClicked = false;
+
   while (!WindowShouldClose()) {
     BeginDrawing();
     ClearBackground(bg1);
+
+    Vector2 mousePoint = GetMousePosition();
+
+#pragma region Sunlight
+    if (isDay) {
+      dayTimer += GetFrameTime();
+      if (dayTimer < dayMinutes / 2) {
+        sunlight += 100.0f / (dayMinutes / 2 * targetFps);
+        if (sunlight > 100.0f)
+          sunlight = 100.0f;
+      } else if (dayTimer > dayMinutes / 2) {
+        sunlight -= 100.0f / (dayMinutes / 2 * targetFps);
+        if (sunlight < 0.1f) {
+          sunlight = 0.1f;
+          dayTimer = 0.0f;
+          isDay = false;
+        }
+      }
+    } else {
+      dayTimer += GetFrameTime();
+      if (dayTimer > nightMinutes) {
+        isDay = true;
+        dayTimer = 0.0f;
+      }
+    }
+
+#pragma region Upper Panel
+    DrawRectangle(0, 0, screenWidth, PanelHeight, bg2);
+    DrawLine(screenWidth / 5, PanelHeight * 0.1, screenWidth / 5,
+             PanelHeight * 0.9, LinesColor);
+    DrawLine(screenWidth / 5 * 2, PanelHeight * 0.1, screenWidth / 5 * 2,
+             PanelHeight * 0.9, LinesColor);
+    DrawLine(screenWidth / 5 * 3, PanelHeight * 0.1, screenWidth / 5 * 3,
+             PanelHeight * 0.9, LinesColor);
+    DrawLine(screenWidth / 5 * 4, PanelHeight * 0.1, screenWidth / 5 * 4,
+             PanelHeight * 0.9, LinesColor);
+
+    // Game Button
+    Rectangle gameButton = {5, PanelHeight * 0.1, screenWidth / 5 - 10,
+                            PanelHeight * 0.8};
+    isHovering = CheckCollisionPointRec(mousePoint, gameButton);
+    isClicked = isHovering && IsMouseButtonPressed(MOUSE_LEFT_BUTTON);
+    DrawRectangleRec(gameButton, isHovering ? HoverOver : bg2);
+
+    DrawText("Game", screenWidth / 5 / 2 - MeasureText("Game", HeaderFont) / 2,
+             PanelHeight / 2 - HeaderFont / 2, HeaderFont, HeaderColor);
+
+    if (isClicked && currentScreen != SCREEN_MAIN) {
+      currentScreen = SCREEN_MAIN;
+    }
+
+    // Upgrades Button
+    Rectangle upgButton = {screenWidth / 5 + 5, PanelHeight * 0.1,
+                           screenWidth / 5 - 10, PanelHeight * 0.8};
+    isHovering = CheckCollisionPointRec(mousePoint, upgButton);
+    isClicked = isHovering && IsMouseButtonPressed(MOUSE_LEFT_BUTTON);
+    DrawRectangleRec(upgButton, isHovering ? HoverOver : bg2);
+
+    DrawText("Upgrades",
+             screenWidth / 5 * 2 - screenWidth / 5 / 2 -
+                 MeasureText("Upgrades", HeaderFont) / 2,
+             PanelHeight / 2 - HeaderFont / 2, HeaderFont, HeaderColor);
+#pragma region Lower Panel
+
 #pragma region SCREEN_MAIN
     if (currentScreen = SCREEN_MAIN) {
     }
