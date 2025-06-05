8962a4562a3954622e6300b8fc611137b1058581
Sinyster <martasko6@protonmail.com>
2025-06-05 19:03:21 +0200

    Base

--- main.c
+++ main.c
@@ -1,11 +1,19 @@
 #include "raylib.h"
 #include "variables.h"
+#include <stdio.h>
 
 // Functions
+// Functions: Render
 void RenderUpperPanel(Color bg);
 void RenderLowerPanel(Color bg);
 
+// Functions: Texts
+void RenderUpperPanelTexts(GameScreen CurrentScreen, Color Active,
+                           Color Inactive);
 int main(void) {
+  // Setting Default Values
+  GameScreen CurrentScreen = SCREEN_PLAY;
+
   InitWindow(ScreenWidth, ScreenHeight, Title);
   SetTargetFPS(TargetFps);
 
@@ -15,6 +23,7 @@ int main(void) {
 
     // Upper Panel
     RenderUpperPanel(PanelBackground);
+    RenderUpperPanelTexts(CurrentScreen, BLACK, GRAY);
 
     // Lower Panel
     RenderLowerPanel(PanelBackground);
@@ -34,5 +43,27 @@ void RenderUpperPanel(Color bg) {
 // Function for Rendering Background of Lower panel
 void RenderLowerPanel(Color bg) {
   DrawRectangle(0, ScreenHeight - PanelHeight, ScreenWidth, PanelHeight, bg);
+  return;
+}
+
+// Function for Rendering Upper Panel Button Texts - Even Active one
+void RenderUpperPanelTexts(GameScreen CurrentScreen, Color Active,
+                           Color Inactive) {
+  char buffer[128];
+  for (int i = 0; i < NumOfUPT; i++) {
+    const char *title = UpperPanelTitles[i];
+    if (i == CurrentScreen) {
+      snprintf(buffer, sizeof(buffer), "*%s*", title);
+    } else {
+      snprintf(buffer, sizeof(buffer), "%s", title);
+    }
+
+    float textWidth = MeasureText(buffer, FontSizeHeader);
+    float x = (ScreenWidth / NumOfUPT) * (i + 0.5f) - textWidth / 2;
+    float y = PanelHeight / 2 - FontSizeHeader / 2;
+
+    DrawText(buffer, x, y, FontSizeHeader,
+             (i == CurrentScreen) ? Active : Inactive);
+  }
   return;
 }
--- variables.h
+++ variables.h
@@ -1,6 +1,14 @@
 #ifndef VAR_H
 #define VAR_H
 
+typedef enum {
+  SCREEN_PLAY,
+  SCREEN_UPGRADE,
+  SCREEN_STATS,
+  SCREEN_SETTINGS,
+  SCREEN_MENU
+} GameScreen;
+
 // Window Variables
 #include "raylib.h"
 const float ScreenWidth = 1280.0f;
@@ -10,9 +18,19 @@ const char Title[8] = "SS-OREH";
 
 // Panel Variables
 const float PanelHeight = 40.0f;
+const float Spacing = 10.0f;
 
 // Defining Colors
 Color BackgroundWhite = WHITE;
 Color PanelBackground = LIGHTGRAY;
+
+// Font Variables
+const float FontSizeHeader = 32.0f;
+const float FontSizeText = 26.0f;
+
+// Titles for Upper Panel
+const char UpperPanelTitles[][24] = {"GAME", "UPGRADES", "STATISTICS",
+                                     "SETTINGS", "MENU"};
+int NumOfUPT = sizeof(UpperPanelTitles) / sizeof(UpperPanelTitles[0]);
 
 #endif
